a313055de29fdc7675fe80dc401ebc04dc3fcc6e
upokusaev <upokusaev@gmail.com>
2019-10-21 08:16:58 +0400

    upd

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const autoprefixer = require('autoprefixer');
 
@@ -9,7 +9,7 @@ const conf = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'main.js',
-    publicPath: 'dist/',
+    // publicPath: 'dist/',
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
