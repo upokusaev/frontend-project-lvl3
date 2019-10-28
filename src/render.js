af2bb8a4a093d4f5b4899a087c2944cbe803a0e0
upokusaev <upokusaev@gmail.com>
2019-10-28 10:41:04 +0400

    refactoring

--- src/render.js
+++ src/render.js
@@ -2,14 +2,11 @@ export const renderFeed = (feed) => {
   const listFeeds = document.querySelector('.list-feeds');
   const newFeed = document.createElement('div');
   newFeed.classList.add('list-group-item');
-  const titleFeed = document.createElement('h5');
-  titleFeed.classList.add('mb-1');
-  titleFeed.textContent = feed.title;
-  const descFeed = document.createElement('p');
-  descFeed.classList.add('mb-1');
-  descFeed.textContent = feed.description;
-  newFeed.appendChild(titleFeed);
-  newFeed.appendChild(descFeed);
+
+  newFeed.innerHTML = `
+    <h5 class="mb-1">${feed.title}</h5>
+    <p class="mb-1">${feed.description}</p>
+  `;
   listFeeds.insertBefore(newFeed, listFeeds.firstChild);
   return feed;
 };
@@ -19,23 +16,16 @@ export const renderNews = (feed) => {
   const news = Object.keys(feed.news);
   news.reverse().forEach((link) => {
     const newsBlock = document.createElement('div');
-    newsBlock.classList.add('list-group-item');
-    const newsLink = document.createElement('a');
-    newsLink.textContent = feed.news[link].title;
-    newsLink.setAttribute('href', link);
-    const button = document.createElement('button');
-    button.textContent = 'Подробнее';
-    button.classList.add('more-about-news', 'btn', 'btn-outline-info');
-    button.setAttribute('type', 'button');
-    button.setAttribute('data-toggle', 'modal');
-    button.setAttribute('data-target', '#mod');
-    button.addEventListener('click', () => {
+    newsBlock.innerHTML = `
+    <div class="list-group-item">
+      <a href="${link}">${feed.news[link].title}</a>
+      <button type="button" class="more-about-news btn btn-outline-info" data-toggle="modal" data-target="#mod">Подробнее</button>
+    </div>`;
+    newsBlock.querySelector('button').addEventListener('click', () => {
       const modal = document.querySelector('#mod');
       modal.querySelector('.modal-title').textContent = feed.news[link].title;
       modal.querySelector('.modal-body').innerHTML = feed.news[link].description;
     });
-    newsBlock.appendChild(newsLink);
-    newsBlock.appendChild(button);
     listNews.insertBefore(newsBlock, listNews.firstChild);
   });
   return feed;
